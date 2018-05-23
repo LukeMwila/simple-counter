dfebfea6c4b9f19c5a61eb4285bb548c678506e7
Luke Mwila <outlier.developer@gmail.com>
2018-05-23 06:21:07 +0200

    Split up the reducers and combined them in the root index file to pass the root reducer to the store

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,40 +1,30 @@
+import * as actionTypes from '../actions'
+
 const initialState = {
-    counter: 0,
-    results: []
+    counter: 0
 }
 
 const reducer = ( state = initialState, action ) => {
     switch( action.type ){
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
             return {
                 ...state,
                 counter: state.counter + 1
             }
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return {
                 ...state,
                 counter: state.counter - 1
             }
-        case 'ADD_COUNTER':
+        case actionTypes.ADD_COUNTER:
             return {
                 ...state,
                 counter: state.counter + action.value
             }
-        case 'SUBTRACT_COUNTER':
+        case actionTypes.SUBTRACT_COUNTER:
             return {
                 ...state,
                 counter: state.counter - action.value
-            }
-        case 'STORE_RESULT':
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: state.counter})
-            }
-        case 'DELETE_RESULT':
-        const updatedArray = state.results.filter(result => result.id !== action.resultElId)
-            return {
-                ...state,
-                results: updatedArray
             }
     }
     return state
